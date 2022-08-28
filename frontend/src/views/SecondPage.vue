3aa896c8531f7e1669fdcd0c64345fda630cd965
j0kene <jolene.yeoo@gmail.com>
2022-08-28 17:58:10 +0800

    second page header

--- frontend/src/views/SecondPage.vue
+++ frontend/src/views/SecondPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="secondPage"><h1>Recommended For You</h1></div>
+  <div class="secondPage">
+    
+    <div class = "header">
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@500&family=Poppins:wght@500&display=swap" rel="stylesheet">
+      <h1>Recommended For You</h1>
+    </div>
+    
+  </div>
 </template>
 
 <script>
